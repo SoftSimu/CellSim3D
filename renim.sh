6541b94ec1bf67599fdc4e7af03ac5203c588ace
Pranav Madhikar <pmadhikar@gmail.com>
2014-09-14 09:28:33 -0400

    Updated render script to fix infinite rendering bug.

--- renim.sh
+++ renim.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
-# First get the trajectories to render
-rm .render_queue &> /dev/null
 shopt -s nullglob
-for file in traj*.xyz; do
-    if [ -z $(grep -o "$file" .render_done &> /dev/null) ]; then
-        echo "$file" >> .render_queue
-    fi
-done
-shopt -u nullglob # revert nullglob back to it's normal default status
-
-while [ TRUE ]; do
-
-    # Render the queued trajectories
-    while read trajFile || [[ -n $line ]]; do
+for trajFile in traj*.xyz; do
+        # Render the queued trajectories
         # Create directory
         trajName="${trajFile%.*}"
         mkdir -p "$trajName/images" # 'already exists' message shouldn't happen
         # Start rendering with blender
-        echo "Now processing $trajName" > .now_proc
         rm "$trajName/images"/*
         echo "Blender is rendering $trajName..."
         echo "you may monitor the $trajName/images folder to see progress"
@@ -28,22 +16,22 @@ while [ TRUE ]; do
         trap "kill $pid 2> /dev/null" EXIT
 
         tmp="unlikely string"
-        echo -ne "|"
+        echo -ne "oOO"
         while kill -0 $pid 2> /dev/null; do
-            echo -ne "\b\r/"
+            echo -ne "\b\b\bOoO"
             sleep 0.2
-            echo -ne "\b\r--"
+            echo -ne "\b\b\bOOo"
             sleep 0.2
-            echo -ne "\b\r\\ "
+            echo -ne "\b\b\bOoO"
             sleep 0.2
-            echo -ne "\b\r|"
+            echo -ne "\b\b\boOO"
             sleep 0.2
 
             newestFile=$(ls -t "$trajName/images/" | head -n 1)
             
             if [ "$tmp" != "$newestFile" -a -n "$newestFile" ]; then
                 echo -e "\b\rCreated $trajName/images/$newestFile"
-                echo -ne "|"
+                echo -ne "oOO"
                 tmp="$newestFile"
             fi
         done
@@ -64,23 +52,6 @@ while [ TRUE ]; do
             echo -ne "\b\r|"
             sleep 0.2
         done
-
-        # Add name to the done file
-
-        echo "$trajFile" >> .render_done
-    done < .render_queue
-
-    # Now check if any new trajectories were created
-    rm .render_queue
-    
-    for file in traj*.xyz; do
-        if [ -z $(grep -o "$file" .render_done) ]; then  # If this file hasn't been processed yet
-                echo "$file" >> .render_queue
-        fi
-    done
-
-    if [ ! -e .render_queue ]; then
-        break
-    fi
-        
+        mv "$trajFile" "$trajFile~"
 done
+shopt -u nullglob # revert nullglob back to it's normal default status
