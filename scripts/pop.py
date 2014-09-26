4f572e25a5970bd0989b0d170e5d685150dcd0d8
Pranav Madhikar <pmadhikar@gmail.com>
2014-09-26 13:32:21 -0400

    Updated pop.py script to make it more usable.

--- scripts/pop.py
+++ scripts/pop.py
@@ -3,24 +3,36 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) < 3:
+    print "Usage: pop.py {Path to trajectory.xyz} {Path to output}"
+    print "Don't put extension in output path."
+    print "e.g."
+    print "pop.py /path/to/trajec.xyz /path/to/image"
+    sys.exit(0)
+
 
 pop=[]
 
-with open('pop.dat', 'w') as popFile:
+with open(sys.argv[1], 'r') as file:
+    line = file.readline()
+    while ( line != ""):
+        #print line
+        nAtoms = int(line.strip())
+        nCells = nAtoms/192
+        step = file.readline().strip()[6:]
+        print "nCells = %d Step = %s" % (nCells, step)
+        pop.append(nCells)
+        for n in xrange(nAtoms): # skip coords + Step line
+            line = file.readline()
+        line = file.readline()
 
-    with open('traj.xyz', 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            if not "C" in line and not ":" in line:
-                pop.append(int(line.strip())/192)
 
 x=np.array(range(len(pop)))
-y=np.array(pop); 
+y=np.array(pop);
 
 plt.plot(x, y, 'k.')
 plt.ylabel('Population')
 plt.xlabel('t')
-plt.savefig('pop.png')
-
-                
-            
+plt.savefig(sys.argv[2] + '.png')
