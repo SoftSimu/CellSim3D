1c441529990212aca0bf29a9f06115bf917353b1
Pranav Madhikar <pmadhikar@gmail.com>
2015-01-29 16:21:58 -0500

    Modify pop.py to make it a little more versatile and powerfull

--- scripts/pop.py
+++ scripts/pop.py
@@ -1,22 +1,47 @@
-#!/usr/bin/ipython
+#!/usr/bin/env python
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import sys
+import sys, os, argparse
+
+
+print "JELLLO PORL"
+
+desc= """
+Plots the population of a simulation versus time.
+"""
+
+parser = argparse.ArgumentParser(description=desc)
+
+parser.add_argument("trajPaths", nargs='+', help="Path to trajectory file(s)")
 
 files = sys.argv[2:]
 
-pop=[]
-P = [65, 70, 75, 80, 85]
-c = 0
-hs=[]
+args = parser.parse_args()
 
-for filea in files:
-    with open(filea, 'r') as file:
+trajPaths = [os.path.abspath(p) for p in args.trajPaths]
+
+GetStorePath = lambda path: os.path.splitext(path)[0]
+
+def PlotPop(trajPath):
+    pop=[]
+    c = 0
+    q, name = os.path.split(trajPath)
+    print "Processing %s" % name
+    path = GetStorePath(trajPath)
+
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
+    path += "/pop.png"
+
+    with open(trajPath, 'r') as trajFile:
         while True:
             #print line
-            line = file.readline()
+            line = trajFile.readline()
 
             if line == "":
                 break
@@ -24,22 +49,20 @@ for filea in files:
             nAtoms = int(line.strip())
             nCells = nAtoms/192
             pop.append(nCells)
-            line = file.readline()
+            line = trajFile.readline().strip()
+            print "on %s" % line
+
             for n in xrange(nAtoms): # skip coords + Step line
-                line = file.readline()
+                line = trajFile.readline()
 
 
     x=np.array(range(len(pop)))
     y=np.array(pop);
-    h, = plt.plot(x,y, '.', label="P_max = %d" % P[c])
-    hs.append(h)
-    c+=1
-    pop = []
 
+    plt.plot(x,y, '.')
+    plt.ylabel('Population')
+    plt.xlabel('t')
+    plt.savefig(path)
 
-plt.ylabel('Population')
-plt.xlabel('t')
-plt.legend(loc=2, prop={'size':10})
-
-
-plt.savefig(sys.argv[1] + '.png')
+for p in trajPaths:
+    PlotPop(p)
