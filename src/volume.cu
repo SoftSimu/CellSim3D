7e2d08f3a7677aa58841e5064177656c2598891c
¨Pranav <pmadhikar@gmail.com>
2015-12-15 11:40:53 +0100

    New volume calculation with test code.
    Added new volume calculation, still being tested. Simulation is slightly
    slower now.

--- src/volume.cu
+++ src/volume.cu
@@ -1,128 +1,179 @@
 #include <cuda.h>
 #include <stdio.h>
 
+
+struct vec{
+    float x;
+    float y;
+    float z; 
+};
+
+
+
+__device__ float dot(float3 a, float3 b){
+    return (a.x*b.x + a.y*b.y + a.z*b.z); 
+}
+
+__device__ float3 cross(float3 a, float3 b){
+    float3 r;
+    r.x = a.y*b.z - b.z*a.y;
+    r.y = a.z*b.x - a.x*b.z;
+    r.z = a.x*b.y - b.y*b.x;
+
+    return r; 
+}
+    
 __global__ void volumes( int No_of_C180s, int *C180_56,
                          float *X,    float *Y,   float *Z,
                          float *CMx , float *CMy, float *CMz, float *vol,
                          char* cell_div, float divVol, bool checkSphericity,
-                         float* areaList)
-{
-__shared__ float locX[192];
-__shared__ float locY[192];
-__shared__ float locZ[192];
-__shared__ float volume;
-__shared__ float area; 
+                         float* areaList){
+    __shared__ float locX[192];
+    __shared__ float locY[192];
+    __shared__ float locZ[192];
+    __shared__ float volume;
+    __shared__ float volume2;
+    __shared__ float area; 
 
-int fullerene = blockIdx.x;
-int tid       = threadIdx.x;
+    int fullerene = blockIdx.x;
+    int tid       = threadIdx.x;
 
-if ( tid < 180 )
-   {
-   locX[tid] = X[192*fullerene+tid] -CMx[fullerene];
-   locY[tid] = Y[192*fullerene+tid] -CMy[fullerene];
-   locZ[tid] = Z[192*fullerene+tid] -CMz[fullerene];
-   }
-
-if ( tid == 0){
-    volume = 0.0f;
-    area = 0.0f;
-}
-
-__syncthreads();
-
-if ( tid < 92 )
-   {
-
-   float avX = 0.0f;
-   float avY = 0.0f;
-   float avZ = 0.0f;
-
-   for ( int i = 0; i < 5; ++i )
-       {
-       avX += locX[C180_56[7*tid+i]];
-       avY += locY[C180_56[7*tid+i]];
-       avZ += locZ[C180_56[7*tid+i]];
-       }
-
-  float avefactor = 0.166666667f;
-  if ( tid < 12 )
-       {
-       avefactor = 0.2f;
-       }
-  else
-       {
-       avX += locX[C180_56[7*tid+5]];
-       avY += locY[C180_56[7*tid+5]];
-       avZ += locZ[C180_56[7*tid+5]];
-       }
-
-   avX *= avefactor;
-   avY *= avefactor;
-   avZ *= avefactor;
-
-   float totvol = 0.0f;
-   float n1 = 0.0f;
-   float n2 = 0.0f;
-   float n3 = 0.0f;
-   float faceArea = 0.0f;
-
-      
-
-   for ( int i = 0; i < 6; ++i ){
-       n1 = (locY[C180_56[7*tid+i+1]]*avZ-avY*locZ[C180_56[7*tid+i+1]])*locX[C180_56[7*tid+i]];
-       n2 = (locZ[C180_56[7*tid+i+1]]*avX-avZ*locX[C180_56[7*tid+i+1]])*locY[C180_56[7*tid+i]];
-       n3 = (locX[C180_56[7*tid+i+1]]*avY-avX*locY[C180_56[7*tid+i+1]])*locZ[C180_56[7*tid+i]];
-       totvol += fabsf(n1+n2+n3);
-       
-       if (checkSphericity){
-       
-           // Get vectors that define a triangle 1, 2
-           float x1 = locX[C180_56[7*tid+i]] - avX;
-           float y1 = locY[C180_56[7*tid+i]] - avY;
-           float z1 = locZ[C180_56[7*tid+i]] - avZ;
-
-           float x2 = locX[C180_56[7*tid+i+1]] - avX;
-           float y2 = locY[C180_56[7*tid+i+1]] - avY;
-           float z2 = locZ[C180_56[7*tid+i+1]] - avZ;
-
-           // now 1 will hold 1X2
-           float xx = y1*z2 - z1*y2;
-           float yy = z1*x2 - x1*z2;
-           float zz = x1*y2 - y1*x2;
-
-           // area of triangle is then 0.5*|1|
-           faceArea += 0.5 * sqrt(xx*xx + yy*yy + zz*zz);
-       }
-   }
-    atomicAdd(&volume, totvol);
-    
-    if (checkSphericity)
-        atomicAdd(&area, faceArea); 
+    if ( tid < 180 ){
+        locX[tid] = X[192*fullerene+tid] -CMx[fullerene];
+        locY[tid] = Y[192*fullerene+tid] -CMy[fullerene];
+        locZ[tid] = Z[192*fullerene+tid] -CMz[fullerene];
     }
 
-__syncthreads();
+    if ( tid == 0){
+        volume = 0.0f;
+        volume2 = 0.0f; 
+        area = 0.0f;
+    }
 
- if ( tid == 0){
-     // if (isnan(vol[fullerene])){
-     //     printf("OH SHIT: nan volume for cell %d", fullerene); 
-     //     asm("trap;");
-     // }
+    __syncthreads();
+
+    if ( tid < 92 ){
+
+        float avX = 0.0f;
+        float avY = 0.0f;
+        float avZ = 0.0f;
+
+        for ( int i = 0; i < 5; ++i ){
+            avX += locX[C180_56[7*tid+i]];
+            avY += locY[C180_56[7*tid+i]];
+            avZ += locZ[C180_56[7*tid+i]];
+        }
+
+        float avefactor = 0.166666667f;
+        if ( tid < 12 )
+        {
+            avefactor = 0.2f;
+        }
+        else
+        {
+            avX += locX[C180_56[7*tid+5]];
+            avY += locY[C180_56[7*tid+5]];
+            avZ += locZ[C180_56[7*tid+5]];
+        }
+
+        avX *= avefactor;
+        avY *= avefactor;
+        avZ *= avefactor;
+
+        float totvol = 0.0f;
+        float totvol2 = 0.0f;
+        float n1 = 0.0f;
+        float n2 = 0.0f;
+        float n3 = 0.0f;
+        float faceArea = 0.0f;
+
+        float face[7*3];
+
+        float3 p0 = make_float3(avX, avY, avZ); 
+        float3 p1, p2;
+
+        for ( int i = 0; i < 6; ++i ){
+            n1 = (locY[C180_56[7*tid+i+1]]*avZ-avY*locZ[C180_56[7*tid+i+1]])*locX[C180_56[7*tid+i]];
+            n2 = (locZ[C180_56[7*tid+i+1]]*avX-avZ*locX[C180_56[7*tid+i+1]])*locY[C180_56[7*tid+i]];
+            n3 = (locX[C180_56[7*tid+i+1]]*avY-avX*locY[C180_56[7*tid+i+1]])*locZ[C180_56[7*tid+i]];
+            totvol += fabsf(n1+n2+n3);
+
+
+            p1.x = locX[C180_56[7*tid+i]];
+            p1.y = locY[C180_56[7*tid+i]];
+            p1.z = locZ[C180_56[7*tid+i]];
+
+            p2.x = locX[C180_56[7*tid+i+1]];
+            p2.y = locY[C180_56[7*tid+i+1]];
+            p2.z = locZ[C180_56[7*tid+i+1]];
+
+            totvol2 += dot(p0, cross(p1, p2)); 
+
+            if (checkSphericity){
+       
+                // Get vectors that define a triangle 1, 2
+                float x1 = locX[C180_56[7*tid+i]] - avX;
+                float y1 = locY[C180_56[7*tid+i]] - avY;
+                float z1 = locZ[C180_56[7*tid+i]] - avZ;
+
+                float x2 = locX[C180_56[7*tid+i+1]] - avX;
+                float y2 = locY[C180_56[7*tid+i+1]] - avY;
+                float z2 = locZ[C180_56[7*tid+i+1]] - avZ;
+
+                p1.x = p1.x - p0.x;
+                p1.y = p1.y - p0.y;
+                p1.z = p1.z - p0.z;
+
+                p2.x = p2.x - p0.x;
+                p2.y = p2.y - p0.y;
+                p2.z = p2.z - p0.z;
+
+                // now 1 will hold 1X2
+                float xx = y1*z2 - z1*y2;
+                float yy = z1*x2 - x1*z2;
+                float zz = x1*y2 - y1*x2;
+
+                // area of triangle is then 0.5*|1|
+                faceArea += 0.5 * sqrt(xx*xx + yy*yy + zz*zz);
+            }
+        }
+        atomicAdd(&volume, totvol);
+        atomicAdd(&volume2, totvol2); 
+    
+        if (checkSphericity)
+            atomicAdd(&area, faceArea); 
+    }
+
+    __syncthreads();
+
+    if ( tid == 0){
+        // if (isnan(vol[fullerene])){
+        //     printf("OH SHIT: nan volume for cell %d", fullerene); 
+        //     asm("trap;");
+        // }
+
+        
      
-     volume = volume/6.0; 
-     vol[fullerene] = volume;
+        volume = volume/6.0; 
+        vol[fullerene] = volume;
+        bool divide = 0; 
      
-     if (volume > divVol){
-         cell_div[fullerene] = 1;
-     }
+        if (volume > divVol){
+            cell_div[fullerene] = 1;
+            //printf("Cell %d volume =%f, volume2=%f \n", fullerene, volume/6.0, volume2/6.0); 
+            //divide = 1; 
+        }
 
-     if (checkSphericity){
-         areaList[fullerene] = area; 
-         float psi = 4.835975862049408 * powf(volume, 2.0/3.0)/area;
-         if ((1.0f - psi) > 0.1)
-             cell_div[fullerene] = 0;
-     }
- }
-
+        if (checkSphericity){
+            areaList[fullerene] = area; 
+            float psi = 4.835975862049408 * powf(volume, 2.0/3.0)/area;
+            if ((1.0f - psi) > 0.1){
+                cell_div[fullerene] = 0;
+                //printf("cell %d division rejected\n", fullerene);
+            }
+        }
+    }
+    
 }
 
 
