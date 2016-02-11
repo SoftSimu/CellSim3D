46f2ad00150e9034c347fdf7690696a0ab1c6642
¨Pranav <pmadhikar@gmail.com>
2016-02-11 12:06:54 +0100

    Add sum operator overload

--- src/VectorFunctions.hpp
+++ src/VectorFunctions.hpp
@@ -3,17 +3,8 @@
 #ifndef DEVICE_FUNCTIONS_CUH
 #define DEVICE_FUNCTIONS_CUH
 
-// __host__ __device__ float dot(float3 a, float3 b);
-
-// __host__ __device__ float3 cross(float3 a, float3 b);
-
-// __host__ __device__ float mag2(float3 a);
-
-// __host__ __device__ float mag(float3 a);
-
-
 __host__ __device__ inline float dot(float3 a, float3 b){
-    return a.x*a.x + a.y*a.y + a.z*a.z; 
+    return a.x*a.x + a.y*a.y + a.z*a.z;
 }
 
 
@@ -24,15 +15,22 @@ __host__ __device__ inline float3 cross(float3 a, float3 b){
     c.y = a.z*b.x - a.x*b.z;
     c.z = a.x*b.y - a.y*b.x;
 
-    return c; 
+    return c;
 }
 
 __host__ __device__ inline float mag2(float3 a){
-    return dot(a,a); 
+    return dot(a,a);
 }
 
 __host__ __device__ inline float mag(float3 a){
     return sqrtf(mag2(a));
+}
+
+
+__host__ __device__ inline float3 operator+(float3 a, float3 b){
+    a.x = a.x + b.x;
+    a.y = a.y + b.y;
+    a.z = a.z + b.z;
 }
 
 #endif // DEVICE_FUNCTIONS_CUH
