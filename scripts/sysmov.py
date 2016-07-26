f126ec787f3170b93eb7124a84ca9236a5893cc7
Pranav Madhikar <pmadhikar@gmail.com>
2016-07-26 14:20:59 +0200

    Update sysmov to new traj reader format

--- scripts/sysmov.py
+++ scripts/sysmov.py
@@ -15,6 +15,10 @@ from moviepy.video.io.bindings import mplfig_to_npimage
 import moviepy.editor as mpy
 import matplotlib.animation
 
+import celldiv as cd
+
+from tqdm import tqdm
+
 desc="""
 Creates snapshots of the movement  of the center of mass of the system of cells.
 """
@@ -46,23 +50,8 @@ parser.add_argument("--res",
 
 args = parser.parse_args()
 
-nAtoms = 0
-msg = 0
-CoMx = 0.0
-CoMy = 0.0
-CoMz = 0.0
-
 CoM = []
-line = 0
-
-CoMxt = []
-CoMyt = []
-CoMzt = []
-CoMxt_cell = []
-CoMyt_cell = []
-CoMzt_cell = []
-c = 0
-
+cellCoMs = []
 trajPath = os.path.abspath(args.trajPath[0])
 
 storPath, trajFileName = os.path.split(trajPath)
@@ -71,67 +60,41 @@ trajFileName = os.path.splitext(trajFileName)[0]
 storPath += "/" + trajFileName + "/" + args.d
 outName = args.out
 
-print "Saving to %s" % storPath
+print ("Saving to %s" % storPath)
 
 try:
     os.makedirs(storPath)
 except:
     pass
 
-with open(trajPath, "r") as trajFile:
-    line = trajFile.readline()
-    while(line != ""):
-        line = line.strip()
-        nAtoms = int(line)
-        step = int(trajFile.readline().strip()[6:])
-        nCells = nAtoms/192
-        print "Processing %d ..." % step
-        CoMxt_cell.append([])
-        CoMyt_cell.append([])
-        CoMzt_cell.append([])
-        for cell in xrange(nCells):
-            for atom in xrange(192):
-                line = trajFile.readline()
-                line = line.strip()
-                line = line.split(',  ');
-                CoMx += float(line[0])
-                CoMy += float(line[1])
-                CoMz += float(line[2])
-
-            CoMx = CoMx / 192
-            CoMy = CoMy / 192
-            CoMz = CoMz / 192
-            CoMxt_cell[c].append(CoMx)
-            CoMyt_cell[c].append(CoMy)
-            CoMzt_cell[c].append(CoMz)
-
-            CoMx = 0.0
-            CoMy = 0.0
-            CoMz = 0.0
-        c += 1
-        line = trajFile.readline()
-
-CoMxt = [sum(a)/len(a) for a in CoMxt_cell]
-CoMyt = [sum(a)/len(a) for a in CoMyt_cell]
-CoMzt = [sum(a)/len(a) for a in CoMzt_cell]
-
-CoMxt = np.array(CoMxt)
-CoMyt = np.array(CoMyt)
-CoMzt = np.array(CoMzt)
+print("Trajectory read progress:")
+with cd.TrajHandle(trajPath) as th:
+    for i in tqdm(range(th.maxFrames)):
+        try:
+            frame = th.ReadFrame()
+            cc = np.array([np.mean(c, axis=0) for c in frame])
+            CoM.append(np.mean(cc, axis=0))
+            cellCoMs.append(cc)
+        except cd.IncompleteTrajectoryError:
+            print("Trajectory is incomplete.")
+            print("Stopping")
+            break
 
 
+CoM = np.array(CoM)
+print("Making plots")
 plt.subplot(2, 2, 1)
-plt.plot(CoMxt, CoMyt, '.')
+plt.plot(CoM[:, 0], CoM[:, 1], '.')
 plt.xlabel("X")
 plt.ylabel("Y")
 
 plt.subplot(2, 2, 2)
-plt.plot(CoMxt, CoMzt, '.')
+plt.plot(CoM[:, 0], CoM[:, 2], '.')
 plt.xlabel("X")
 plt.ylabel("Z")
 
 plt.subplot(2, 2, 3)
-plt.plot(CoMyt, CoMzt, '.')
+plt.plot(CoM[:, 1], CoM[:, 2], '.')
 plt.xlabel("Y")
 plt.ylabel("Z")
 
@@ -139,25 +102,21 @@ plt.tight_layout()
 plt.savefig(storPath + "COM.png")
 plt.clf()
 
-
 fig = plt.figure()
 fps = args.fps
-nFrames = CoMxt.size
+nFrames = CoM.shape[0]
 tPerFrame = 1.0/fps
 dur = (nFrames-1)*tPerFrame
 s = args.s
 
 ax = fig.add_subplot(1, 1, 1, projection='3d')
-xMax = s*max(np.ceil(max(max(CoMxt_cell))),
-             abs(np.floor(min(min(CoMxt_cell)))))
+xMax = s*np.max(CoM[:, 0])
 xMin = -1 * xMax
 
-yMax = s*max(np.ceil(max(max(CoMyt_cell))),
-             abs(np.floor(min(min(CoMyt_cell)))))
+yMax = s*np.max(CoM[:, 1])
 yMin = -1 * yMax
 
-zMax = s*max(np.ceil(max(max(CoMzt_cell))),
-             abs(np.floor(min(min(CoMzt_cell)))))
+zMax = s*np.max(CoM[:, 2])
 zMin = -1 * zMax
 
 ax.set_xlim3d([xMax, xMin])
@@ -167,15 +126,15 @@ ax.set_zlim3d([zMax, zMin])
 def make3DFrame(i):
     i = np.floor(i/tPerFrame)
 
-    ax.scatter3D(CoMxt[i], CoMyt[i], CoMzt[i], '.')
+    ax.scatter3D(CoM[i, 0], CoM[i, 1], CoM[i, 2], '.')
     return mplfig_to_npimage(fig)
 
 def make3DFrameWithCells(i):
     i = int(np.floor(i/tPerFrame))
     ax.cla()
-    ax.scatter3D(CoMxt[i], CoMyt[i], CoMzt[i], '.')
-    ax.scatter3D(CoMxt_cell[i], CoMyt_cell[i], CoMzt_cell[i], c='red', alpha=0.5,
-               marker='.')
+    ax.scatter3D(CoM[i, 0], CoM[i, 1], CoM[i, 2], '.')
+    ax.scatter3D(cellCoMs[i][:, 0], cellCoMs[i][:, 1], cellCoMs[i][:, 2],
+                 c='red', alpha=0.5, marker='.')
 
     ax.set_xlim3d([xMax, xMin])
     ax.set_ylim3d([yMax, yMin])
@@ -184,13 +143,12 @@ def make3DFrameWithCells(i):
 
 
 # This condition is false if there is more than one cell in the system
-if len(CoMxt_cell[-1]) > 1:
+if CoM.shape[0] > 1:
     frameFunc = make3DFrameWithCells
 else:
     frameFunc = make3DFrame
 
-print "Generating Gifs..."
-print "3D..."
+print ("Making movie...")
 
 animation = mpy.VideoClip(frameFunc, duration=dur)
 animation.write_videofile(storPath + outName, fps=fps)
