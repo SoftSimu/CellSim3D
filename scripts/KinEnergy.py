9bd7f4533165543491303df4814d0f233cef49de
Pranav Madhikar <pmadhikar@gmail.com>
2016-11-29 23:47:35 +0100

    Add script to plot kinetic energy

---
+++ scripts/KinEnergy.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.style
+matplotlib.style.use('ggplot')
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+import celldiv as cd
+import argparse
+from tqdm import tqdm
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+
+#parser.add_argument("traj", type=str)
+
+parser.add_argument("-k", "--skip",
+                    help="Trajectory frame skip rate",
+                    type = int,
+                    default = 1)
+
+#parser.add_argument('dt', type=float)
+
+parser.add_argument('csv')
+
+
+args=parser.parse_args()
+
+# t = 0
+# dt = args.skip*args.dt
+# with cd.TrajHandle(args.traj) as th:
+#     t = dt*(int(th.maxFrames/args.skip))
+#     print(dt, t)
+#     keList = [0 for i in range(int(th.maxFrames/args.skip))]
+#     f1 = np.vstack(th.ReadFrame(inc=args.skip))
+#     f1 -= np.mean(f1, axis=0)
+#     for i in tqdm(range(int(th.maxFrames/args.skip))):
+#         frame = np.vstack(th.ReadFrame(inc=args.skip))
+#         frame -= np.mean(frame, axis=0)
+#         Vs = (frame[:f1.shape[0]] - f1)/dt
+#         keList[i] = 0.5*np.sum(np.linalg.norm(Vs, axis=1)**2)
+#         f1 = np.copy(frame)
+
+
+# plt.plot(keList, lw=1.5, label="calculated")
+
+
+d = pd.read_csv(args.csv)[["step", "V"]].as_matrix()
+keList2 = []
+for s in tqdm(range(0, int(d[:,0].max()), 100*args.skip)):
+    V = d[d[:, 0] == s, 1]
+    keList2.append(np.sum(0.5*V**2))
+
+plt.plot(keList2, lw=1.5, label="read")
+plt.gca().set_yscale('log')
+plt.legend()
+plt.savefig('ke.png')
