a265e8f637c85753f3e78663629ff23eb6cabe63
Pranav Madhikar <pmadhikar@gmail.com>
2017-07-10 00:39:30 +0200

    Change scripts/xyzafy.py to allow outputting only one specified frame

--- scripts/xyzafy.py
+++ scripts/xyzafy.py
@@ -13,7 +13,7 @@ parser.add_argument("traj", type=str)
 parser.add_argument("-o", "--out", type=str, default="out.xyz")
 parser.add_argument("-k", "--skip", type=int, default=1)
 parser.add_argument("-m", "--max", type=int, default=float("inf"))
-
+parser.add_argument("-f", "--frame", type=int, default=-1)
 args = parser.parse_args()
 
 trajPath = os.path.abspath(args.traj)
@@ -27,22 +27,29 @@ def GetMaxPart():
     print("System contains {0} particles".format(n*180))
     return n*180
 
-n = GetMaxPart()
-filler = "C  0.0  0.0  0.0\n"
-with cd.TrajHandle(trajPath) as th, open(args.out, "w") as outHandle:
-    for i in tqdm(range(min(args.max, int(th.maxFrames/args.skip)))):
-        frame = th.ReadFrame(inc=args.skip)
-        nP = len(frame)*180
-        outHandle.write("%d\n" % n)
-        outHandle.write("%d, t = %d\n" % (nP, th.currFrameNum))
-        CoM = np.mean(np.vstack(frame), axis=0)
-        for c in frame:
-            c = c[:-12] - CoM
-            for p in c.tolist():
-                outHandle.write("C  ")
-                outHandle.write("  ".join([str(a) for a in p]))
-                outHandle.write("\n")
+def WriteFrame(frame, n):
+    nP = len(frame)*180
+    outHandle.write("%d\n" % n)
+    outHandle.write("%d, t = %d\n" % (nP, th.currFrameNum))
+    CoM = np.mean(np.vstack(frame), axis=0)
+    for c in frame:
+        c = c[:-12] - CoM
+        for p in c.tolist():
+            outHandle.write("C  ")
+            outHandle.write("  ".join([str(a) for a in p]))
+            outHandle.write("\n")
 
         if nP < n:
             for _ in range(n - nP):
                 outHandle.write(filler)
+
+n = GetMaxPart()
+filler = "C  0.0  0.0  0.0\n"
+with cd.TrajHandle(trajPath) as th, open(args.out, "w") as outHandle:
+    if args.frame != -1:
+        frame = th.ReadFrame(frameNum=args.frame)
+        WriteFrame(frame, len(frame)*180)
+    else:
+        for i in tqdm(range(min(args.max, int(th.maxFrames/args.skip)))):
+            frame = th.ReadFrame(inc=args.skip)
+            WriteFrame(frame, n)
