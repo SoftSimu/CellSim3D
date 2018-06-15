cd8d82077e40b2dbf74aa58af8cbfbc2565869a5
Pranav Madhikar <pmadhikar@gmail.com>
2018-06-15 16:46:44 +0200

    Add SimList1D constructor that accepts a default value

--- inc/SimList.cuh
+++ inc/SimList.cuh
@@ -25,6 +25,12 @@ struct SimList1D: base_n{
         devPtr = thrust::raw_pointer_cast(&d[0]);
         hostPtr = thrust::raw_pointer_cast(&h[0]);
     }
+    
+    SimList1D(size_t _n, T _val): n(_n), h(_n, _val), d(_n, _val){
+        base_n::used_host_mem += n*sizeof(T);
+        devPtr = thrust::raw_pointer_cast(&d[0]);
+        hostPtr = thrust::raw_pointer_cast(&h[0]);
+    }
 
     ~SimList1D(){
         base_n::used_host_mem -= n*sizeof(T);
