479c1610f30523bb88933fd23bb82980bea202d1
evitali <emanuele.vitali@csc.fi>
2024-05-22 14:20:05 +0300

    fix missing header in hipify

--- hipify.sh
+++ hipify.sh
@@ -2,5 +2,7 @@
 
 sed -i 's/"trap;"/"s_trap 0x02;"/g' src/volume.cu
 sed -i 's/"trap;"/"s_trap 0x02;"/g' src/propagate.cu
-find . -type f \( -iname \*.cu -o -iname \*.cuh -o -iname \*.cpp -o -iname \*.hpp -o -iname \*.h -o -iname *.cxx \) -exec hipify-perl -inplace -experimental -print-stats {} \;
+#find . -type f \( -iname \*.cu -o -iname \*.cuh -o -iname \*.cpp -o -iname \*.hpp -o -iname \*.h -o -iname *.cxx \) -exec hipify-perl -inplace -experimental -print-stats {} \;
+
+sed -i '1s/^/#include \"hip\/hip_runtime.h\" /' src/postscriptinit.cu 
 
