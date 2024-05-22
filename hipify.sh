1c40dac100cee222679d47b9c7eaa0f7935f34de
evitali <emanuele.vitali@csc.fi>
2024-05-22 13:57:28 +0300

    first attempt

---
+++ hipify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i 's/"trap;"/"s_trap 0x02;"/g' src/volume.cu
+sed -i 's/"trap;"/"s_trap 0x02;"/g' src/propagate.cu
+find . -type f \( -iname \*.cu -o -iname \*.cuh -o -iname \*.cpp -o -iname \*.hpp -o -iname \*.h -o -iname *.cxx \) -exec hipify-perl -inplace -experimental -print-stats {} \;
+
